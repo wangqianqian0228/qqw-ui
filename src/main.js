41939ce3e0a4b0af3e73edaa3f59c18d7f8aa3ab
wangqianqian <1848105094@qq.com>
2021-12-06 20:38:27 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import qqwUi from './packages/index'
 Vue.config.productionTip = false
-
+Vue.use(qqwUi)
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/packages/index.js
@@ -0,0 +1,7 @@
+import Button from './button'
+const install = (Vue) => {
+  Vue.component('qqw-button',Button)
+}
+export {
+  install
+}
