2eececa95f2deeeaed74175ebcb6fe465e06ac8d
wangqianqian <1848105094@qq.com>
2021-12-23 20:52:38 +0800

    初始化

--- src/packages/index.js
+++ src/packages/index.js
@@ -5,8 +5,8 @@ const install = (Vue) => {
 if(typeof window.Vue !== 'undefined'){
   install(Vue)
 }
-/* <script src="Vue"></script>
-这里引用的Vue会挂载到全局上，而`import Vue from 'vue'`这里的Vue会在当前模块中。 */
+{/* <script src="Vue"></script>
+这里引用的Vue会挂载到全局上，而`import Vue from 'vue'`这里的Vue会在当前模块中。 */}
 export default {
   install
 }
