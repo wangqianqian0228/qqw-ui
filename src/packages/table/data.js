009ac77c4a65fd94649355604643792b77cb59ea
wangqianqian <1848105094@qq.com>
2022-11-08 11:18:42 +0800

    用render函数渲染表格，实现编辑和保存功能

--- src/packages/table/data.js
+++ src/packages/table/data.js
@@ -1,71 +1,3 @@
-let columns = [{
-        title: '姓名',
-        key: 'name'
-    },
-    {
-        title: '年龄',
-        key: 'age'
-    },
-    // render函数写好了，咋用呢？
-    // 这里的render是一个普通的函数
-    // @params h createElement
-    // @params 第二个参数，是从render.js传过来的对象
-
-    {
-        title: '出生日期',
-        render: (h, {
-            row
-        }) => {
-            const date = new Date(parseInt(row.birthday));
-            const year = date.getFullYear();
-            const month = date.getMonth() + 1;
-            const day = date.getDate();
-            const birthday = `${year}-${month}-${day}`;
-            return h('span', birthday);
-        }
-    },
-    {
-        title: '地址',
-        key: 'address'
-    }, {
-        title: '操作',
-        render: (h, {
-            row,
-            column,
-            index,
-            that
-        }) => {
-            if (index === that.editIndex) {
-                // 处于编辑状态
-                // 创建一个input，值改为当前行的值
-                return [
-                    h('button', '保存', {
-                        on: {
-                            click: () => {
-                                
-                            }
-                        }
-                    }),
-                    h('button', '取消')
-
-                ]
-            } else {
-                // 渲染编辑和取消按钮
-                return h('button', {
-                    on: {
-                        click: () => {
-                            that.editName = row.name
-                            that.editAge = row.age
-                            that.editBirthday = row.birthday
-                            that.address = row.address
-                            that.editIndex = index
-                        }
-                    }
-                }, '修改')
-            }
-        }
-    }
-]
 let data = [{
         name: '王小明',
         age: 18,
@@ -91,6 +23,197 @@ let data = [{
         address: '深圳市南山区深南大道'
     }
 ]
+let columns = [{
+        title: '姓名',
+        key: 'name',
+        render: (h, {
+            row,
+            index,
+            that
+        }) => {
+            let edit = '';
+            if (that.editIndex === index) {
+                // 处于编辑状态时
+                edit = [
+                    h('input', {
+                        style: {
+                            color: 'red'
+                        },
+                        //dom原生属性
+                        domProps: {
+                            value: row.name
+                        },
+                        on: {
+                            input: (e) => {
+                                that.editName = e.target.value
+                            }
+                        }
+                    })
+                ]
+            } else {
+                edit = row.name
+            }
+            return h('div', [edit])
+        }
+    },
+    {
+        title: '年龄',
+        key: 'age',
+        render: (h, {
+            row,
+            index,
+            that
+        }) => {
+            let edit = '';
+            if (that.editIndex === index) {
+                // 处于编辑状态时
+                edit = [
+                    h('input', {
+                        style: {
+                            color: 'red'
+                        },
+                        //dom原生属性
+                        domProps: {
+                            value: row.age
+                        },
+                        on: {
+                            input: (e) => {
+                                that.editAge = e.target.value
+                            }
+                        }
+                    })
+                ]
+            } else {
+                edit = row.age
+            }
+            return h('div', [edit])
+        }
+    },
+    // render函数写好了，咋用呢？
+    // 这里的render是一个普通的函数
+    // @params h createElement
+    // @params 第二个参数，是从render.js传过来的对象
+
+    {
+        title: '出生日期',
+        render: (h, {
+            row, index,
+            that
+        }) => {
+            const date = new Date(parseInt(row.birthday));
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1;
+            const day = date.getDate();
+            const birthday = `${year}-${month}-${day}`;
+
+            let edit = '';
+            if (that.editIndex === index) {
+                // 处于编辑状态时,日期可以用日期插件
+                edit = [
+                    h('input', {
+                        style: {
+                            color: 'red'
+                        },
+                        //dom原生属性
+                        domProps: {
+                            value: birthday
+                        },
+                        on: {
+                            input: (e) => {
+                                console.log('输入框的值', e.target.value)
+                                that.editBirthday = new Date(e.target.value).value()
+                            }
+                        }
+                    })
+                ]
+            } else {
+               edit = birthday
+            }
+            return h('div', [edit])
+        }
+    },
+    {
+        title: '地址',
+        key: 'address',
+        render: (h, {
+            row,
+            index,
+            that
+        }) => {
+            let edit = '';
+            if (that.editIndex === index) {
+                // 处于编辑状态时
+                edit = [
+                    h('input', {
+                        style: {
+                            color: 'red'
+                        },
+                        //dom原生属性
+                        domProps: {
+                            value: row.address
+                        },
+                        on: {
+                            input: (e) => {
+                                that.editAddress = e.target.value
+                            }
+                        }
+                    })
+                ]
+            } else {
+                edit = row.address
+            }
+            return h('div', [edit])
+        }
+    }, {
+        title: '操作',
+        render: (h, {
+            row,
+            column,
+            index,
+            that
+        }) => {
+            if (index === that.editIndex) {
+                // 处于编辑状态
+                // 创建一个input，值改为当前行的值
+                return [
+                    h('button', {
+                        on: {
+                            click: () => {
+                                data[index].name = that.editName
+                                data[index].age = that.editAge
+                                data[index].birthday = that.editBirthday
+                                data[index].address = that.editAddress
+                                that.editIndex = -1
+                            }
+                        }
+                    }, '保存', ),
+                    h('button', {
+                        on: {
+                            click: ()=>{
+                                that.editIndex = -1
+                            }
+                        }
+                    },'取消')
+
+                ]
+            } else {
+                // 渲染编辑和取消按钮
+                return h('button', {
+                    on: {
+                        click: () => {
+                            that.editName = row.name
+                            that.editAge = row.age
+                            that.editBirthday = row.birthday
+                            that.editAddress = row.address
+                            that.editIndex = index
+                        }
+                    }
+                }, '修改')
+            }
+        }
+    }
+]
+
 export {
     columns,
     data
